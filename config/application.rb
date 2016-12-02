30349a3db9a5a4f31ebe723ee43425f49eb3bf5a
rikushingaki <shingakiriku@outlook.jp>
2016-12-02 00:15:49 +0000

    add top page

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,7 @@ module Microposts
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    config.generaters do |g|
+    config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
