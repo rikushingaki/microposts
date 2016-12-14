86473ac4219726a54cf621f513251e80acca5049
rikushingaki <shingakiriku@outlook.jp>
2016-12-14 11:54:10 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,:location,
+    params.require(:user).permit(:name, :email, :password, :location,
                                  :password_confirmation)
   end
   
